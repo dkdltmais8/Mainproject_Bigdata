9b8eff92d0fb08e3f08e0880561cfaf5a39c7d88
Hyungsik <shs950930@gmail.com>
2021-09-19 13:57:27 +0900

    Feat: 메인페이지틀 구현(미완성)
    Former-commit-id: efdf45ddcc7a3185ea851bc7e9db533eef4b137f

--- FrontEnd/src/pages/Auth/Survey.js
+++ FrontEnd/src/pages/Auth/Survey.js
@@ -1,13 +1,24 @@
-import React, { Component }from "react";
+import React from "react";
+import Card from '@material-ui/core/Card';
 
-class Survey extends Component {
-  render() {
+
+function Survey( {history} ){
     return (
         <div>
-            <h3> here survey page </h3>
+          <h3> here survey page </h3>
+          <div>
+            <p>선호 지역을 선택해주세요</p>
+            <button>미국</button>
+            <button>중국</button>
+            <button>한국</button>
+            <button>일본</button>
+          </div>
+          <div>
+            <p>시청한 영화를 평가해주세요</p>
+            <Card variant="outlined">a</Card>
+          </div>
         </div>
     );
-  }
 }
 
 export default Survey;
