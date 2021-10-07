2395989d97278fdf673eb71cf5feb347081628c5
김지안 <jiahn.dev@gmail.com>
2021-10-07 15:06:10 +0900

    Fix: 프론트 백 연결경로변경
    Former-commit-id: de6098754216dad539a43659fe69c76fdb9f5510

--- BackEnd/FrontEnd/src/pages/Auth/Survey.js
+++ BackEnd/FrontEnd/src/pages/Auth/Survey.js
@@ -10,7 +10,7 @@ import Spinner from '../../components/Spinner.js';
 
 function Survey( {history} ){
   const [movies,setMovies] = useState([])
-  const url = "http://localhost:8000/movie/survey"
+  const url = "/movie/survey"
   const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
@@ -71,7 +71,7 @@ function Survey( {history} ){
 
   const submitEvent =() =>{
     setLoading(true);
-    axios.post("http://localhost:8000/accounts/survey",{
+    axios.post("/accounts/survey",{
       result:result,
       id:localStorage.getItem("id")
     })
